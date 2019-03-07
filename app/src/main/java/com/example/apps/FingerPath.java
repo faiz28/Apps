f851a3280e07e81f3e28a4d1ffb75751e4b3a15b
Faiz Ahmed <Faizcse28@gmail.com>
2019-03-07 16:48:29 +0600

    Children apps

---
+++ app/src/main/java/com/example/apps/FingerPath.java
@@ -0,0 +1,19 @@
+package com.example.apps;
+import android.graphics.Path;
+
+
+public class FingerPath {
+    public int color;
+    public boolean emboss;
+    public  boolean blur;
+    public int strokWidth;
+    public Path path;
+
+    public FingerPath(int color, boolean emboss, boolean blur, int strokWidth,Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokWidth = strokWidth;
+        this.path=path;
+    }
+}
---
+++ app/src/main/java/com/example/apps/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.apps;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private PaintView paintView;
+    private Button reset,normal,blur,emboss;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        paintView=(PaintView) findViewById(R.id.Draw);
+        DisplayMetrics metrics=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+
+        normal=(Button) findViewById(R.id.normal_button);
+        normal.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paintView.normal();
+            }
+        }));
+
+        blur=(Button) findViewById(R.id.Blur_button);
+        blur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paintView.blur();
+            }
+        });
+
+        reset=(Button) findViewById(R.id.reset_button);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paintView.clear();
+            }
+        });
+
+        emboss=(Button) findViewById(R.id.Black_button);
+        emboss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paintView.emboss();
+            }
+        });
+
+
+    }
+
+
+}
